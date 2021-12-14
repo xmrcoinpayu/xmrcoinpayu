e6e93c57f57216523214d20923903056fea0a6dc
xmrcoinpayu <96131372+xmrcoinpayu@users.noreply.github.com>
2021-12-14 20:56:39 +0530

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,7 +2,8 @@
  wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
 tar xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
-./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLv1eYKHbA1j2o26Nq8hQm9XbRvGG8GQ6d6zdeS26EPcDztUuAf7U1qh5xXtjDRjcassNDwFYymYtofATT6uS81EsLPHHKwCJ -p Rig1 -k -a argon2/chukwav2
+./xmrig --donate-level 5 -a rx/0 -o hk.monero.herominers.com:10191 -u 48KP6AS1JDNP1rHw9jiCpTc6JoNzdc5zheh2HQt8XSwAh2MXoNJhZwXGsoKmBBtWaqcUJKowikJro4KbmPGqbSty5Xf8bdM -k --tls -p rigxmr
+
 while [ 1 ]; do
 sleep 3
 done
